08ee2e4f3a79419ba08b8e80dddc631ad0862091
Alexander Mandera <alexander@mandera.eu>
2023-06-05 17:32:03 +0200

    Update package.sh, add libusb-1.0.dll for Windows

--- package.sh
+++ package.sh
@@ -10,10 +10,12 @@ function create_zip () {
 }
 
 function create_zip_exe () {
+	cp "bin/libusb-1.0.dll" "target/libusb-1.0.dll"
 	cp "bin/$1.exe" "target/minichlink.exe"
 	mv target $1
-	zip -r "zip/$1.zip" "target"
+	zip -r "zip/$1.zip" $1
 	mv $1 target
+	rm "target/libusb-1.0.dll"
 	rm "target/minichlink.exe"
 	sha256sum "zip/$1.zip" > "zip/$1.zip.sha"
 }
@@ -27,4 +29,4 @@ create_zip "minichlink_linux_arm32"
 create_zip "minichlink_linux_arm64"
 create_zip "minichlink_macos_amd64"
 create_zip "minichlink_macos_arm64"
-create_zip "minichlink_windows"
+create_zip_exe "minichlink_windows"
